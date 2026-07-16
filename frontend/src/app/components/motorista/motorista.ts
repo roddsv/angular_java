31a6de84ed0ac2afb1d382d5e11499e683b1720c
Rodrigo Soares <adsrodsoares@gmail.com>
2026-07-16 18:40:23 -0300

    Componente motorista - carregando e aceitando corridas

--- frontend/src/app/components/motorista/motorista.ts
+++ frontend/src/app/components/motorista/motorista.ts
@@ -1,9 +1,33 @@
-import { Component } from '@angular/core';
-
+import { Component, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { RideService } from '../../services/ride';
+import { Ride } from '../../models/ride';
 @Component({
   selector: 'app-motorista',
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './motorista.html',
   styleUrl: './motorista.css',
 })
-export class DriverComponent {}
+export class DriverComponent {
+  private rideService = inject(RideService);
+
+  motoristaId = 3;
+
+  availableRides: Ride[] = [];
+  activeRide: Ride | null = null;
+
+  getAvailableRides(): void {
+    this.availableRides = [
+      {id: 10, userId: 3, passageiroId: 1, origem: 'Rua A', destino: 'Rua B', status: 'SOLICITADO'}
+    ];
+  }
+
+  ngOnInit(): void {
+    this.getAvailableRides();
+  }
+
+  acceptRide(rideId: number): void {
+    console.log(`Motorista ${this.motoristaId} aceitou a corrida ${rideId}`);
+  }
+}
